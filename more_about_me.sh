18b627aeac187588d9a97e8be5c36390c80cbf0e
Thinesh Subramani <thineshsubramani@gmail.com>
2024-12-26 15:06:14 +0800

    Create more_about_me.sh

---
+++ more_about_me.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Function to print a header with green color
+print_header() {
+    echo -e "\n\033[1;32m************* $1 *************\033[0m\n"
+}
+
+# Function to print bold green text
+print_bold() {
+    echo -e "\033[1;32m$1\033[0m"
+}
+
+# Introduction
+print_header "Introduction"
+print_bold "I am passionate about contributing to organizations that value innovation and human creativity."
+
+# Professional Journey
+print_header "Professional Journey"
+print_bold "I have transitioned from a Software Engineer to DevOps, and currently to a Site Reliability Engineer (SRE), building upon a strong foundation in codin>
+
+# Future Plans
+print_header "Future Plans"
+print_bold "I have dedicated several years to selecting the appropriate research path, not merely for academic advancement."
+print_bold "My goal is to explore new system design methodologies for large-scale microservices."
+print_bold "I aim to pursue a Master's degree in distributed computing, focusing on large-scale system designs and microservices."
+
+# Personal Interests in SRE
+print_header "Personal Interests in SRE"
+print_bold "I enjoy simulating my own homelab and managing SRE setups, including configuring Proxmox virtual machines."
+
+# Areas of Lesser Interest
+print_header "Areas of Lesser Interest"
+print_bold "While I am expanding my knowledge in monitoring tools like Grafana and have experience with ELK, I have less interest in security-related tasks and fr>
+
+# Personality and Working Style
+print_header "Personality and Working Style"
+print_bold "- As an INFJ, 'The Advocate,' I prioritize delivering results efficiently, adhering to the 80/20 principle."
+print_bold "- I prefer to automate repetitive tasks to enhance productivity, exemplified by creating a Python CLI tool for sending emails."
+print_bold "- I value trust in my ideas and proactively address organizational challenges by developing proofs of concept."
